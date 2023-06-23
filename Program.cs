39b1c968cd1d59b60a7b2d273bc3cd6a6fa37021
Sadaleksej <135204154+Sadaleksej@users.noreply.github.com>
2023-06-23 17:09:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+int[] arrr = new int[30];
+FilArray(arrr);
+PrintArray(arrr);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов на нечетных позициях: {SumUnEven(arrr)}");
+
+void FilArray(int[] ar)
+{
+    for (int i = 0; i < ar.Length; i++) ar[i] = new Random().Next(1, 99);
+}
+
+void PrintArray(int[] ar)
+{
+    for (int i = 0; i < ar.Length; i++) Console.Write(ar[i] + " ");
+}
+int SumUnEven(int[] ar)
+{
+    int sum = 0;
+
+  for (int i = 0; i<ar.Length; i++) 
+    {
+    if(i % 2 != 0) sum = sum + ar[i];
+    }
+return sum;
+}
